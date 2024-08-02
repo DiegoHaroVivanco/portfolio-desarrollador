707fa3c9e37c440dbcc516638630a39761c099ca
Diego Haro Vivanco <diego.harovivanco@gmail.com>
2024-08-02 20:01:39 -0300

    se agrego un header, cambio de iconos

---
+++ components/header.tsx
@@ -0,0 +1,45 @@
+import Link from "next/link";
+import MotionTransition from "./transition-component";
+import { socialNetworks } from "@/data";
+
+const Header = () =>{
+
+    return(
+        <MotionTransition position="bottom" className="absolute z-40 inline-block  w-full top-5 md:top-10" >
+            <header>
+                <div className="container justify-between  max-w-6xl mx-auto md:flex">
+                    <Link href="/" >
+                        {/* para que sea responsive el text-center */}
+                        <h1 className="my-3 text-4xl font-bold text-center md:text-left"> 
+                            Diego
+                            <span className="text-secondary">
+                                Haro
+                            </span>
+                        
+                        </h1>
+                    
+                    </Link>
+                    <div className="flex items-center justify-center gap-7">
+                        {socialNetworks.map(({logo, src, title,id}) =>(
+                            <Link key={id}
+                                href={src}
+                                target="_blank"
+                                className=" social-link transition-all duration-300 hover:text-secondary"
+                            >
+                                <span className="flex items-center justify-center">
+                                    {logo}
+                                </span>
+                                {title}
+                            </Link>
+                        ))}
+                    </div>
+
+                </div>
+            </header>
+
+        </MotionTransition>
+        
+    );
+}
+
+export default Header;
